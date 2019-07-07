c23c9419a6f7e4271949a622f105719e617db653
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-07 17:47:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -162,6 +162,8 @@ function moveDodger(e) {
      moveDodgerLeft()
    } else if (e.which === RIGHT_ARROW) {
      moveDodgerRight()
+     e.preventDefault();
+     e.stopPropagation();
    } else if (e.which !== LEFT_ARROW && e.which !== RIGHT_ARROW) {
      continue
    }
