ca7c86aa0e9d477c336c7ac090381d231f85e365
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-07 17:14:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ function createRock(x) {
    */
 
    GAME.append(rock);
-   window.requestAnimationFrame(moveRock);
+   moveRock();
 
   /**
    * This function moves the rock. (2 pixels at a time
