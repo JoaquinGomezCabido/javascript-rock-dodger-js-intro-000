a963f8b783d16b76916bb91d6f76edf4bc37b128
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-07 17:09:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ function createRock(x) {
 
   // We should kick of the animation of the rock around here
 
-  rock.requestAnimationFrame(setInterval);
+//  rock.requestAnimationFrame(setInterval);
 
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision
